b6ec05e266576bfb0201403fe6f1b5207b9608af
treyzania <treyzania@gmail.com>
2017-02-24 01:30:38 -0500

    Added very basic file for vanilla Minecraft.

---
+++ public/minecraft-vanilla/MC1.11.2/pkgconf.py
@@ -0,0 +1,13 @@
+#cripackage minecraft-vanilla MC1.11.2
+
+def install(ctx):
+	# TODO Download server jar.
+	pass
+
+def configure(ctx):
+	# TODO Request configuration options.
+	pass
+
+def start(ctx):
+	# TODO Specify Jar path to executor.
+	pass
