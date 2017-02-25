f4d54fc23cdf41b2ee98dd7b3f2ab722926c423a
treyzania <treyzania@gmail.com>
2017-02-24 20:56:51 -0500

    Made us download the Jar file.

--- public/minecraft-vanilla/MC1.11.2/pkgconf.py
+++ public/minecraft-vanilla/MC1.11.2/pkgconf.py
@@ -1,8 +1,17 @@
 #cripackage minecraft-vanilla MC1.11.2
 
+urltemplate = "https://s3.amazonaws.com/Minecraft.Download/versions/{0}/minecraft_server.{0}.jar"
+version = "1.11.2"
+destination = "server.jar"
+
+def __url():
+	global urltemplate
+	global version
+	return urltemplate.format(version)
+
 def install(ctx):
-	# TODO Download server jar.
-	pass
+	global destination
+	ctx.download_url(__url(), destination)
 
 def configure(ctx):
 	# TODO Request configuration options.
